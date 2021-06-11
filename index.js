6464e78413a774b05659460de76a48f2b683309d
sbb3391 <sbb33391@gmail.com>
2021-06-11 09:02:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  console.log(`This Saturday, I want to ${activity}`); 
+}
+
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}`
+} 
+
+mondayWork();
+
+function wrapAdjective(flair ="*") {
+  return function(quality = "special") {
+    return `You are ${flair}${quality}${flair}!`
+  }
+}
+
+wrapAdjective("$$")("a great guy");
